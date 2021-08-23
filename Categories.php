0729f2875e9b90e4c9a2f18e7c7cca045c9d67ff
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-08-23 13:42:30 +0200

    added edit and delete buttons to client site cards

--- Categories.php
+++ Categories.php
@@ -21,12 +21,16 @@ include('./components/headDash.php')
                             <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="w-full h-full object-center object-cover">
                         </div>
                         <h3 class="mt-6 text-sm text-gray-500">
-                            <a href="#">
+                            <a href="single.php">
                                 <span class="absolute inset-0"></span>
                                 Desk and Office
                             </a>
                         </h3>
                         <p class="text-base font-semibold text-gray-900">Work from home accessories</p>
+                        <div class="flex justify-between md:space-x-20 space-y-10 md:space-y-0 mt-5">
+                            <button class="bg-red-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-red-600 rounded-full hover:shadow-2xl hover:bg-red-400">Delete</button>
+                            <button class="bg-yellow-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-yellow-600 rounded-full hover:shadow-2xl hover:bg-yellow-400">Edit</button>
+                        </div>
                     </div>
 
                     <div class="group relative py-10">
@@ -40,6 +44,10 @@ include('./components/headDash.php')
                             </a>
                         </h3>
                         <p class="text-base font-semibold text-gray-900">Journals and note-taking</p>
+                        <div class="flex justify-between md:space-x-20 space-y-10 md:space-y-0 mt-5">
+                            <button class="bg-red-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-red-600 rounded-full hover:shadow-2xl hover:bg-red-400">Delete</button>
+                            <button class="bg-yellow-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-yellow-600 rounded-full hover:shadow-2xl hover:bg-yellow-400">Edit</button>
+                        </div>
                     </div>
 
                     <div class="group relative py-10">
@@ -53,6 +61,10 @@ include('./components/headDash.php')
                             </a>
                         </h3>
                         <p class="text-base font-semibold text-gray-900">Daily commute essentials</p>
+                        <div class="flex justify-between md:space-x-20 space-y-10 md:space-y-0 mt-5">
+                            <button class="bg-red-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-red-600 rounded-full hover:shadow-2xl hover:bg-red-400">Delete</button>
+                            <button class="bg-yellow-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-yellow-600 rounded-full hover:shadow-2xl hover:bg-yellow-400">Edit</button>
+                        </div>
                     </div>
 
                     <div class="group relative py-10">
@@ -66,6 +78,10 @@ include('./components/headDash.php')
                             </a>
                         </h3>
                         <p class="text-base font-semibold text-gray-900">Work from home accessories</p>
+                        <div class="flex justify-between md:space-x-20 space-y-10 md:space-y-0 mt-5">
+                            <button class="bg-red-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-red-600 rounded-full hover:shadow-2xl hover:bg-red-400">Delete</button>
+                            <button class="bg-yellow-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-yellow-600 rounded-full hover:shadow-2xl hover:bg-yellow-400">Edit</button>
+                        </div>
                     </div>
 
                     <div class="group relative py-10">
@@ -79,6 +95,10 @@ include('./components/headDash.php')
                             </a>
                         </h3>
                         <p class="text-base font-semibold text-gray-900">Work from home accessories</p>
+                        <div class="flex justify-between md:space-x-20 space-y-10 md:space-y-0 mt-5">
+                            <button class="bg-red-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-red-600 rounded-full hover:shadow-2xl hover:bg-red-400">Delete</button>
+                            <button class="bg-yellow-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-yellow-600 rounded-full hover:shadow-2xl hover:bg-yellow-400">Edit</button>
+                        </div>
                     </div>
 
                     <div class="group relative py-10">
@@ -92,6 +112,10 @@ include('./components/headDash.php')
                             </a>
                         </h3>
                         <p class="text-base font-semibold text-gray-900">Work from home accessories</p>
+                        <div class="flex justify-between md:space-x-20 space-y-10 md:space-y-0 mt-5">
+                            <button class="bg-red-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider  text-red-600 rounded-full hover:shadow-2xl hover:bg-red-400">Delete</button>
+                            <button class="bg-yellow-300 px-5 py-3 text-sm shadow-sm font-medium tracking-wider text-yellow-600 rounded-full hover:shadow-2xl hover:bg-yellow-400">Edit</button>
+                        </div>
                     </div>
                 </div>
             </div>
