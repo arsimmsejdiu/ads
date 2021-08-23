4cfe368c17649d71ab1a3027fd9cd5ee27001a68
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-08-23 16:32:40 +0200

    Little changes

--- sections/MediumCard.php
+++ sections/MediumCard.php
@@ -6,8 +6,7 @@
                 <!--First card-->
                 <div class="md:p-8 p-2 bg-white">
                     <!--Banner image-->
-                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
-          " />
+                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
 
                     <!--Tag-->
                     <p class="text-indigo-500 font-semibold text-base mt-2">Science</p>
@@ -42,8 +41,7 @@
                 <!--First card-->
                 <div class="md:p-8 p-2 bg-white">
                     <!--Banner image-->
-                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
-          " />
+                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
 
                     <!--Tag-->
                     <p class="text-indigo-500 font-semibold text-base mt-2">Science</p>
@@ -78,8 +76,113 @@
                 <!--First card-->
                 <div class="md:p-8 p-2 bg-white">
                     <!--Banner image-->
-                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
-          " />
+                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+
+                    <!--Tag-->
+                    <p class="text-indigo-500 font-semibold text-base mt-2">Science</p>
+                    <!--Title-->
+                    <h1 class="font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate">
+                        the life of albert einstein
+                    </h1>
+                    <!--Description-->
+                    <div class="max-w-full">
+                        <p class="text-base font-medium tracking-wide text-gray-600 mt-1">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+                            vel soluta dolore id nesciunt eum nam ipsam, eveniet cupiditate sint
+                            veritatis harum odit. Iste dignissimos, ad provident nulla
+                            voluptatum ut.
+                        </p>
+                    </div>
+                    <div class="flex items-center space-x-2 mt-5">
+                        <!--Author's profile photo-->
+                        <img class="w-10 h-10 object-cover object-center rounded-full" src="assets/images/hacker.png" alt="random user" />
+                        <div>
+                            <!--Author name-->
+                            <p class="text-gray-900 font-semibold">Arsim Sejdiu</p>
+                            <p class="text-gray-500 font-semibold text-sm">
+                                &middot; Feb 24, 2021 &middot;
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!--End of first card-->
+            </a>
+
+            <a href="./single.php">
+                <!--First card-->
+                <div class="md:p-8 p-2 bg-white">
+                    <!--Banner image-->
+                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+
+                    <!--Tag-->
+                    <p class="text-indigo-500 font-semibold text-base mt-2">Science</p>
+                    <!--Title-->
+                    <h1 class="font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate">
+                        the life of albert einstein
+                    </h1>
+                    <!--Description-->
+                    <div class="max-w-full">
+                        <p class="text-base font-medium tracking-wide text-gray-600 mt-1">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+                            vel soluta dolore id nesciunt eum nam ipsam, eveniet cupiditate sint
+                            veritatis harum odit. Iste dignissimos, ad provident nulla
+                            voluptatum ut.
+                        </p>
+                    </div>
+                    <div class="flex items-center space-x-2 mt-5">
+                        <!--Author's profile photo-->
+                        <img class="w-10 h-10 object-cover object-center rounded-full" src="assets/images/hacker.png" alt="random user" />
+                        <div>
+                            <!--Author name-->
+                            <p class="text-gray-900 font-semibold">Arsim Sejdiu</p>
+                            <p class="text-gray-500 font-semibold text-sm">
+                                &middot; Feb 24, 2021 &middot;
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!--End of first card-->
+            </a>
+            <a href="./single.php">
+                <!--First card-->
+                <div class="md:p-8 p-2 bg-white">
+                    <!--Banner image-->
+                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+
+                    <!--Tag-->
+                    <p class="text-indigo-500 font-semibold text-base mt-2">Science</p>
+                    <!--Title-->
+                    <h1 class="font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate">
+                        the life of albert einstein
+                    </h1>
+                    <!--Description-->
+                    <div class="max-w-full">
+                        <p class="text-base font-medium tracking-wide text-gray-600 mt-1">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+                            vel soluta dolore id nesciunt eum nam ipsam, eveniet cupiditate sint
+                            veritatis harum odit. Iste dignissimos, ad provident nulla
+                            voluptatum ut.
+                        </p>
+                    </div>
+                    <div class="flex items-center space-x-2 mt-5">
+                        <!--Author's profile photo-->
+                        <img class="w-10 h-10 object-cover object-center rounded-full" src="assets/images/hacker.png" alt="random user" />
+                        <div>
+                            <!--Author name-->
+                            <p class="text-gray-900 font-semibold">Arsim Sejdiu</p>
+                            <p class="text-gray-500 font-semibold text-sm">
+                                &middot; Feb 24, 2021 &middot;
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!--End of first card-->
+            </a>
+            <a href="./single.php">
+                <!--First card-->
+                <div class="md:p-8 p-2 bg-white">
+                    <!--Banner image-->
+                    <img class="rounded-lg w-full" src="https://images.unsplash.com/photo-1603349206295-dde20617cb6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
 
                     <!--Tag-->
                     <p class="text-indigo-500 font-semibold text-base mt-2">Science</p>
--- signup.php
+++ signup.php
@@ -13,7 +13,7 @@ include('./components/head.php')
                             <img class="w-32 h-32 mr-4 object-cover" src="https://images.unsplash.com/photo-1611867967135-0faab97d1530?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1352&amp;q=80" alt="Avatar Upload">
                         </div>
                         <label class="cursor-pointer ">
-                            <span class="focus:outline-none text-white text-sm py-2 px-4 rounded-full bg-green-400 hover:bg-green-500 hover:shadow-lg">Browse</span>
+                            <span class="focus:outline-none text-white text-sm py-2 px-4 rounded-full bg-green-400 hover:bg-green-500 hover:shadow-lg">Image Profile</span>
                             <input type="file" class="hidden" :multiple="multiple" :accept="accept">
                         </label>
                     </div>
